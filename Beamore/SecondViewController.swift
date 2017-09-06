cace9229d55dfe228fa58f959c8627f196512978
Onur Celik <onrcelik@outlook.com>
2017-09-06 18:30:42 +0300

    Initial Commit

---
+++ Beamore/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Beamore
+//
+//  Created by Onur Celik on 06/09/2017.
+//  Copyright © 2017 Onur Celik. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
